45f1b246618c485230cc30fe1e0d5fb9c2781e62
Informatyk1234 <kp300350@student.polsl.pl>
2025-04-28 15:31:13 +0200

    Add dash character instead of blank cell, add mock data

--- client/src/components/tables/UsersTable.tsx
+++ client/src/components/tables/UsersTable.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+
+// Returns dash, if value is null
+export function renderValueOrDash(params: GridRenderCellParams) {
+  return params.value ?? '-';
+}
+
+// Returns dash, if date is null
+export function renderDateOrDash(params: GridRenderCellParams) {
+  return params.value ? new Date(params.value).toLocaleDateString() : '-';
+}
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -8,12 +18,13 @@ const columns: GridColDef[] = [
   { field: 'lastName', headerName: 'Nazwisko', width: 130 },
   { field: 'role', headerName: 'Rodzaj konta', width: 130, },
   { field: 'status', headerName: 'Status', width: 130, },
-  { field: 'adress', headerName: 'Adres', width: 160, },
-  { field: 'apartementNumber', headerName: 'Numer mieszkania', type: 'number', align: 'left', width: 140, },
-  { field: 'moveinDate', headerName: 'Data wprowadzenia', type: 'date', width: 160, },
-  { field: 'moveoutDate', headerName: 'Data wyprowadzenia', type: 'date', width: 160, },
+  { field: 'adress', headerName: 'Adres', width: 160, renderCell: renderValueOrDash },
+  { field: 'apartementNumber', headerName: 'Numer mieszkania', type: 'number', align: 'left', width: 140, renderCell: renderValueOrDash },
+  { field: 'moveinDate', headerName: 'Data wprowadzenia', type: 'date', width: 160, renderCell: renderDateOrDash },
+  { field: 'moveoutDate', headerName: 'Data wyprowadzenia', type: 'date', width: 160, renderCell: renderDateOrDash },
 ];
 
+// Random mock data
 const rows = [
   { id: 1, 
     lastName: 'Snow', 
@@ -24,14 +35,82 @@ const rows = [
     apartementNumber: 23,
     moveinDate: new Date('2022-03-22'),
     moveoutDate: new Date('2044-12-11') },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { 
+      id: 2, 
+      lastName: 'Kowalski', 
+      firstName: 'Adam', 
+      role: 'Menadżer', 
+      status: 'Aktywny',
+      adress: null,
+      apartementNumber: null,
+      moveinDate: null,
+      moveoutDate: null
+  },
+  { 
+      id: 3, 
+      lastName: 'Nowak', 
+      firstName: 'Anna', 
+      role: 'Mieszkaniec', 
+      status: 'Nieaktywny',
+      adress: 'Jasnogórska 4',
+      apartementNumber: 2,
+      moveinDate: new Date('2020-08-01'),
+      moveoutDate: new Date('2024-02-22') 
+  },
+  { 
+      id: 4, 
+      lastName: 'Szymański', 
+      firstName: 'Marek', 
+      role: 'Menadżer', 
+      status: 'Aktywny',
+      adress: null,
+      apartementNumber: null,
+      moveinDate: null,
+      moveoutDate: null
+  },
+  { 
+      id: 5, 
+      lastName: 'Wiśniewski', 
+      firstName: 'Michał', 
+      role: 'Najemca', 
+      status: 'Aktywny',
+      adress: 'Wielka 28',
+      apartementNumber: 5,
+      moveinDate: new Date('2023-01-10')
+  },
+  { 
+      id: 6, 
+      lastName: 'Zieliński', 
+      firstName: 'Karolina', 
+      role: 'Menadżer', 
+      status: 'Aktywny',
+      adress: null,
+      apartementNumber: null,
+      moveinDate: null,
+      moveoutDate: null
+  },
+  { 
+      id: 7, 
+      lastName: 'Mazur', 
+      firstName: 'Paweł', 
+      role: 'Mieszkaniec', 
+      status: 'Nieaktywny',
+      adress: 'Zielona 9',
+      apartementNumber: 12,
+      moveinDate: new Date('2018-03-10'),
+      moveoutDate: new Date('2022-07-30') 
+  },
+  { 
+      id: 8, 
+      lastName: 'Jankowski', 
+      firstName: 'Tomasz', 
+      role: 'Najemca', 
+      status: 'Aktywny',
+      adress: 'Lwowska 13',
+      apartementNumber: 9,
+      moveinDate: new Date('2022-07-15'),
+      moveoutDate: new Date('2031-09-20') 
+  }
 ];
 
 const paginationModel = { page: 0, pageSize: 5 };
