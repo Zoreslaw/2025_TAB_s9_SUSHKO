78420cc5af280b0eebf5b46b0814b3ea550caaa2
Tymek <clipper460@gmail.com>
2025-04-30 18:22:08 +0200

    feat: added object panel with pagitation and modals

--- client/src/pages/Adminpage.tsx
+++ client/src/pages/Adminpage.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+//import ObjectPanel from './admin/ObjectPanel';
 
 const AdminPage: React.FC = () => {
-  return <div>Admin Page</div>;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/object');
+  };
+
+  return (
+    <div>
+      <h2>Admin Page</h2>
+      <Button variant="contained" color="primary" onClick={handleNavigate}>
+        Go to Object Panel
+      </Button>
+      
+    </div>
+
+    
+  );
 };
 
 export default AdminPage;
+
